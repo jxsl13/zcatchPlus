f7f33a34e6bdfca29bb9fe8a3199277981178e28
John Behm <john@Johns-MBP.fritz.box>
2018-08-23 20:26:09 +0200

    added byobu checks

--- restart.sh
+++ restart.sh
@@ -24,9 +24,30 @@ sleep 1
 echo "shutdown" > fifofile
 fi
 
-if [ -f /usr/bin/byobu ]; then 
+if [ $(which byobu) ]; then 
 byobu kill-session -t "zcatch"
 byobu new-session -d -s "zcatch" "./start.sh"
 else
 echo "Byobu does not exist - please install byobu"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # ...
+        sudo apt install byobu
+
+        elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        brew install byobu
+
+        #elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        #elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        #elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+        #elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+        else
+                echo "Not supported operating system."
+        fi
 fi
+
