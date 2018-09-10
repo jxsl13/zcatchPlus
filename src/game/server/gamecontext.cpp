888907bb0e50a09e01e5cfddbb53365aa5fc4b2b
John Behm <john@Johns-MBP.fritz.box>
2018-09-10 22:12:41 +0200

    leaving players are automatically vote banned for the remaining time of 60 seconds between failing votes

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1017,6 +1017,14 @@ void CGameContext::OnClientDrop(int ClientID, const char *pReason)
 
 	m_BotDetection->OnPlayerDisconnect(ClientID);
 
+	// if player voted something/someone and it did not pass
+	// before leaving the server, voteban him for the remaining time.
+	int64 Now = Server()->Tick();
+	int Timeleft = m_apPlayers[ClientID]->m_LastVoteCall + Server()->TickSpeed() * 60 - Now;
+	if (m_apPlayers[ClientID]->m_LastVoteCall && Timeleft > 0)
+	{
+		GetBanServer()->Server()->AddVoteban(ClientID, Timeleft);
+	}
 
 	AbortVoteKickOnDisconnect(ClientID);
 	m_apPlayers[ClientID]->OnDisconnect(pReason);
