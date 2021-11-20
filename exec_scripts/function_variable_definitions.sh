104856d166644e3e42ec378e97a57ff57e76a302
jxsl13 <jxsl13@gmail.com>
2021-11-20 12:21:21 +0100

    update building

--- exec_scripts/function_variable_definitions.sh
+++ exec_scripts/function_variable_definitions.sh
@@ -51,36 +51,11 @@ check_build_bam (){
 	if [ ! -f ../bam/bam ]; then
     # Download and compile bam:
     cd ..
-    git clone https://github.com/matricks/bam.git bam
-    git checkout -b 0.5 origin/0.5
+    git clone https://github.com/jxsl13/bam.git bam
     cd bam
-
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # ...
-        chmod +x make_unix.sh
-        ./make_unix.sh
-        chmod +x bam
-
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        chmod +x make_unix.sh
-        ./make_unix.sh
-        chmod +x bam
-
-	#elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-	#elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-	#elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-	#elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
-	else
-        echo "Not supported operating system."
-	fi
-
+    git checkout fix-macos-raise
+    make
 	back_to_base_directory
-
 fi
 }
 
